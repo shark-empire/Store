e9fbd28068d3dc020c89e5c688e18de7a2539b41
Shark-empire <babor3331@icloud.com>
2026-06-30 19:50:55 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+
+// Import your existing pages
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Marketplace from './pages/Marketplace';
+import StoreBuilder from './pages/StoreBuilder';
+import StoreView from './pages/StoreView';
+import Stores from './pages/Stores';
+import BundleDetail from './pages/BundleDetail';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <main className="pt-[70px]"> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="/stores" element={<Stores />} />
+          <Route path="/store-builder" element={<StoreBuilder />} />
+          <Route path="/store-view/:id" element={<StoreView />} />
+          <Route path="/bundle/:id" element={<BundleDetail />} />
+        </Routes>
+      </main> 
+    </div>
+  );
+}
+
+export default App;
