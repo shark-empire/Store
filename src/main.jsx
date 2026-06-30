62a324366fc6f35c3a312ac3d95314b074ff2034
Shark-empire <babor3331@icloud.com>
2026-06-30 20:14:27 +0000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import { AuthProvider } from './contexts/AuthContext'; // 1. Import your provider
+import eruda from 'eruda'; // 2. Import eruda
+
+eruda.init(); // 3. Initialize eruda
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <AuthProvider> {/* 4. Wrap App with the Provider */}
+        <App />
+      </AuthProvider>
+    </HashRouter>
+  </React.StrictMode>
+);
